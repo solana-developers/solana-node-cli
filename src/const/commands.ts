c643ea3650989cf74df5a3b3112a2faf1b1031f0
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2024-10-15 09:23:48 -0400

    feat: common options

--- src/const/commands.ts
+++ src/const/commands.ts
@@ -1,14 +1,29 @@
 import { Option } from "@commander-js/extra-typings";
+import { DEFAULT_CONFIG_FILE, DEFAULT_KEYPAIR_PATH } from "./solana";
 
 /**
  * Listing of the common and reusable command options
  */
 export const COMMON_OPTIONS = {
+  /**
+   * path to the local Solana.toml config file
+   *
+   * note: this is a different config file than the solana cli's config file
+   */
   config: new Option(
     "-c --config <path>",
     "path to a Solana.toml config file",
-  ).default("temp/Solana.toml"),
-  // .default("Solana.toml"), // todo: use this
+  ).default(DEFAULT_CONFIG_FILE),
+  /**
+   * path to the local authority keypair
+   */
+  authority: new Option(
+    "--keypair <path>",
+    "path to a Solana.toml config file",
+  ).default(DEFAULT_KEYPAIR_PATH),
+  /**
+   * rpc url or moniker to use
+   */
   url: new Option(
     "-u --url <URL_OR_MONIKER>",
     "URL for Solana's JSON RPC or moniker",
---
+++ src/const/solana.ts
@@ -0,0 +1,5 @@
+// todo: remove the temp
+// export const DEFAULT_CONFIG_FILE = "Solana.toml";
+export const DEFAULT_CONFIG_FILE = "temp/Solana.toml";
+
+export const DEFAULT_KEYPAIR_PATH = "~/.config/solana/id.json";
