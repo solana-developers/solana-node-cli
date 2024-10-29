89c612b663594dae201e070f0063c34889146567
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2024-10-28 21:02:19 -0400

    feat: safe rpc moniker

--- src/lib/solana.ts
+++ src/lib/solana.ts
@@ -1,7 +1,7 @@
 import { Keypair } from "@solana/web3.js";
 import { doesFileExist, loadJsonFile } from "./utils";
 import { DEFAULT_KEYPAIR_PATH } from "@/const/solana";
-import { SolanaCluster } from "@/types/config";
+import { ProgramBuildLabels, SolanaCluster } from "@/types/config";
 import { warnMessage } from "./cli";
 
 export function loadKeypairFromFile(
@@ -15,25 +15,55 @@ export function loadKeypairFromFile(
 /**
  * Parse the provided url to correct it into a valid moniker or rpc url
  */
-export function parseRpcUrlOrMoniker(url: string): SolanaCluster | string {
-  if (url.match(/^http?s/i)) {
+export function parseRpcUrlOrMoniker(
+  input: string,
+  allowUrl: boolean = true,
+  includeBetaLabel: boolean = false,
+): SolanaCluster | string {
+  if (allowUrl && input.match(/^http?s/i)) {
     try {
-      return new URL(url).toString();
+      return new URL(input).toString();
     } catch (err) {
-      console.error("Unable to parse 'url':", url);
+      console.error("Unable to parse 'url':", input);
       process.exit(1);
     }
-    return url;
-  } else if (url.startsWith("local") || url.startsWith("l")) {
+    return input;
+  } else if (input.startsWith("local") || input.startsWith("l")) {
     return "localhost";
-  } else if (url.startsWith("t")) {
+  } else if (input.startsWith("t")) {
     return "testnet";
-  } else if (url.startsWith("d")) {
+  } else if (input.startsWith("d")) {
     return "devnet";
-  } else if (url.startsWith("m")) {
-    return "mainnet-beta";
+  } else if (input.startsWith("m")) {
+    return includeBetaLabel ? "mainnet-beta" : "mainnet";
   } else {
     warnMessage("Unable to parse url or moniker. Falling back to mainnet");
-    return "mainnet-beta";
+    return includeBetaLabel ? "mainnet-beta" : "mainnet";
   }
+}
+
+export function safeCheckClusterLabel(
+  labels: ProgramBuildLabels,
+  cluster: SolanaCluster,
+): false | keyof ProgramBuildLabels {
+  // allow equivalent cluster names
+  switch (cluster) {
+    case "localhost":
+    case "localnet": {
+      cluster = "localnet";
+      break;
+    }
+    case "mainnet":
+    case "mainnet-beta": {
+      cluster = "mainnet";
+      break;
+    }
+    //  we do not need to handle these since there is not a common equivalent
+    // case "devnet":
+    // case "testnet":
+    // default:
+  }
+
+  if (Object.hasOwn(labels, cluster)) return cluster;
+  else return false;
 }
